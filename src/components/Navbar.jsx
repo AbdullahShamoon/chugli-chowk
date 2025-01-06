522ca3f298d10e38b7726d8d8a2aec328c4e08e8
Abdullah <abdullahshamoon24@gmail.com>
2025-01-06 13:55:55 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
         <nav className='flex justify-center text-center p-3 bg-[#94949433] text-white text-3xl font-bold m-3 rounded-full shadow-lg relative gap-2'>
-            <img src="public/images/chat.png" alt="" className='absolute top-4 left-4 w-9'/>
+            <img src="/images/chat.png" alt="" className='absolute top-4 left-4 w-9'/>
         <span>Chugli Chowk </span> <span><img src="/images/speak.png" alt="" width={31} /></span>
         </nav>
     </>
