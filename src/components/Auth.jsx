faacdffdca284409539494db3236a4c6b5b1043b
Abdullah <abdullahshamoon24@gmail.com>
2025-01-06 13:55:28 +0530

    minor fix

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -21,7 +21,7 @@ const Auth = (props) => {
     <>
       <div className="flex flex-col justify-center items-center text-2xl font-bold h-[50vh]">
 
-        <button onClick={googleSignIn} className="flex justify-center items-center px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-400 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+        <button onClick={googleSignIn} className="justify-center items-center px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-400 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
           <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
           <span>Sign In with Google</span>
         </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <>
         <nav className='flex justify-center text-center p-3 bg-[#94949433] text-white text-3xl font-bold m-3 rounded-full shadow-lg relative gap-2'>
             <img src="public/images/chat.png" alt="" className='absolute top-4 left-4 w-9'/>
-        <span>Chugli Chowk </span> <span><img src="public/images/speak.png" alt="" width={31} /></span>
+        <span>Chugli Chowk </span> <span><img src="/images/speak.png" alt="" width={31} /></span>
         </nav>
     </>
   )
